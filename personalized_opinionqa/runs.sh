827af3a3db07a6cb7de2c338703b4a04ad328388
Niket Tandon <nikett@allenai.org>
2023-04-20 08:34:39 -0700

    explicit vs implicit for 100 users

---
+++ personalized_opinionqa/runs.sh
@@ -0,0 +1,3 @@
+python personalized_opinionqa/personalized_opinionqa.py --option 0
+python personalized_opinionqa/personalized_opinionqa.py --option 1
+python personalized_opinionqa/personalized_opinionqa.py --option 2
