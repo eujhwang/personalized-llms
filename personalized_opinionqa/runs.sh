4ea7fb89e27727766e80f373fcdc88d805967db3
Niket Tandon <nikett@allenai.org>
2023-05-02 17:08:30 -0700

    experiments to predict demography and ideology from opinions

--- personalized_opinionqa/runs.sh
+++ personalized_opinionqa/runs.sh
@@ -21,4 +21,7 @@
 #python personalized_opinionqa/personalized_opinionqa.py --option -1  --num_implicit 8 --max_users 35 --max_ques 30 --max_topics -1
 
 #python personalized_opinionqa/personalized_opinionqa.py --option 2  --num_implicit 16 --max_users 35 --max_ques 30 --max_topics -1
-python personalized_opinionqa/personalized_opinionqa.py --option 0  --num_implicit 16 --max_users 35 --max_ques 30 --max_topics -1
+#python personalized_opinionqa/personalized_opinionqa.py --option 0  --num_implicit 16 --max_users 35 --max_ques 30 --max_topics -1
+
+python personalized_opinionqa/demography_prediction.py --predict "ideo" --num_implicit 16 --max_users 35 --max_topics -1
+#python personalized_opinionqa/demography_prediction.py --predict "ideo" --num_implicit 8 --max_users 35 --max_topics -1
