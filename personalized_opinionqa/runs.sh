485514e275bddb8aaec136ff6eda7587596b5d3b
Niket Tandon <nikett@allenai.org>
2023-05-04 01:00:15 -0700

    group level and population level accuracies

--- personalized_opinionqa/runs.sh
+++ personalized_opinionqa/runs.sh
@@ -23,5 +23,11 @@
 #python personalized_opinionqa/personalized_opinionqa.py --option 2  --num_implicit 16 --max_users 35 --max_ques 30 --max_topics -1
 #python personalized_opinionqa/personalized_opinionqa.py --option 0  --num_implicit 16 --max_users 35 --max_ques 30 --max_topics -1
 
-python personalized_opinionqa/demography_prediction.py --predict "ideo" --num_implicit 16 --max_users 35 --max_topics -1
+#python personalized_opinionqa/demography_prediction.py --predict "ideo" --num_implicit 16 --max_users 35 --max_topics -1
 #python personalized_opinionqa/demography_prediction.py --predict "ideo" --num_implicit 8 --max_users 35 --max_topics -1
+
+
+#python personalized_opinionqa/mean_responses_qa.py --experiment_type "ideology:Republican"
+#python personalized_opinionqa/mean_responses_qa.py --experiment_type "ideology:Democrat"
+#python personalized_opinionqa/mean_responses_qa.py --experiment_type "ideology:Independent"
+#python personalized_opinionqa/mean_responses_qa.py --experiment_type "overall"
